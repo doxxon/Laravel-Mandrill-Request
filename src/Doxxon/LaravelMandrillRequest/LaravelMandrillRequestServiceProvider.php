adc6799ba91e7c55328ba66935c8a0aa40bf61d0
Mark <marek.szymeczko@polcode.net>
2015-01-13 12:02:55 +0100

    PSR-2 standard coding implemented

--- src/Doxxon/LaravelMandrillRequest/LaravelMandrillRequestServiceProvider.php
+++ src/Doxxon/LaravelMandrillRequest/LaravelMandrillRequestServiceProvider.php
@@ -2,46 +2,44 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelMandrillRequestServiceProvider extends ServiceProvider {
+class LaravelMandrillRequestServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('doxxon/laravel-mandrill-request');
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('doxxon/laravel-mandrill-request');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['mandrillrequest'] = $this->app->share(function ($app) {
+            return new MandrillRequest();
+        });
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['mandrillrequest'] = $this->app->share(function($app)
-		{
-		    return new MandrillRequest;
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
